f8c397cf8dcc66a58801f85343836ef08c9bf88e
ahmednagra <ahmednagra9@gmail.com>
2023-02-25 02:40:04 +0500

    YOutube admin Fix

--- testrecorder/youtube/admin.py
+++ testrecorder/youtube/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from youtube.models import UserYoutubePlaylists, ChannelsRecord, PlaylistsRecord
 
-from youtube.models import UserYoutubePlaylists,ChannelsRecord
-
+"""
 admin.site.register(UserYoutubePlaylists)
 admin.site.register(ChannelsRecord)
+
+"""
+
+""" Muhammad Ahmed  """
+@admin.register(UserYoutubePlaylists)
+class UserYoutubePlaylistsAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'playlist_id',
+                    'playlist_title', 'playlist_enabled')
+
+
+@admin.register(PlaylistsRecord)
+class PlaylistsRecordAdmin(admin.ModelAdmin):
+    list_display = ('playlist_id', 'playlist_title', 'timestamp')
+
+
+@admin.register(ChannelsRecord)
+class ChannelsRecordAdmin(admin.ModelAdmin):
+    list_display = ('channel_id', 'channel_title',
+                    'channel_credentials', 'timestamp')
