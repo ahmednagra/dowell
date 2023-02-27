f0e115081f2f229ec5049639131994a70aeed7e0
Uzair Tahir <uzairtahir535@gmail.com>
2023-02-27 17:29:01 +0500

    error

--- testrecorder/static/js/index2.js
+++ testrecorder/static/js/index2.js
@@ -1,17 +1,13 @@
 
 var sharebtn= document.querySelector("#screen-share");
 var screenReco =document.getElementById("screen_recording")
-var cameraCheckbox = document.getElementById('webcam-recording')
+var webcam = document.getElementById('webcam-recording')
 var recbtn= document.querySelector("#record");
 var stopbtn= document.querySelector("#stop");
 const videoElement = document.getElementById('video');
 var downloadlink = document.querySelector("#downloadlink");
 var error = document.querySelector('#error').innerHTML;
-//var mic = document.querySelector('#audio-settings');
-// for mic constrainits
-/* let screenAudioConstraints = {
-  audio: true
-}; */ 
+
 //let screenStream = null;
 // Custom function
 
@@ -27,20 +23,14 @@ error="Welcome Sharing Portal ";
 
 //Options from Users 
 // Voice enable/Disable 
-var microphoen_btn = null;
-async function micphone_status(){
-  microphoen_btn= document.getElementById("audio-settings");
-  if (microphoen_btn.checked == true) {
-    return microphoen_btn = true;
-  } else {
-   return microphoen_btn = false;
-  }
-  //console.log("microphoen btn value" , microphoen_btn);
+async function micphoneStatus(screenAudioConstraints, screenStream){
+ 
+  return stream;
 }
 //Webcam Enable/disable
 var webcam_status_btn = null;
 async function webcam_status(){
-  webcam_status_btn= cameraCheckbox;
+  webcam_status_btn= webcam;
   if (webcam_status_btn.checked == true) {
     return webcam_status_btn = true;
   } else {
@@ -60,7 +50,27 @@ async function computerScreen_status(){
 }
 
 
+// Gets computer/capture screen stream 
+async function computerscreen(mediaConstraints = {
+  video: {
+    cursor: 'always',
+    resizeMode: 'crop-and-scale'
+  },
+  //audio: true
+}) {
 
+  try {
+    const screenStream = await navigator.mediaDevices.getDisplayMedia(mediaConstraints)
+    return screenStream
+  }
+  catch (err) {
+    let msg = "STATUS: Error while getting screen stream."
+    document.getElementById("app-status").innerHTML = msg;
+    alert("Error while getting screen stream!\n -Please share screen when requested.\n -Try to start the recording again.");
+    // Tell user, stop the recording.
+    resetStateOnError();
+  }
+}
 
 // Gets webcam/camera stream
 async function captureMediaDevices(currentMediaConstraints) {
@@ -79,27 +89,6 @@ async function captureMediaDevices(currentMediaConstraints) {
         }
       }
 }
-// Gets computer/capture screen stream 
-async function computerscreen(){
-  if(computerScreen_btn == true){
-  mediaConstraints = {
-    video: {
-      cursor: 'always',
-      resizeMode: 'crop-and-scale'
-    },
-    //audio: true
-    } 
-  try { // phly screen stream ko global null kiya
-    const screenStream = await navigator.mediaDevices.getDisplayMedia(mediaConstraints)
-    return screenStream
-  }
-  catch (err) {
-    let msg = "STATUS: Error while getting screen stream."
-    document.getElementById("error").innerHTML = msg;
-  }
-}
-}
- 
 
 
 // Records webcam and audio
@@ -227,103 +216,63 @@ async function recordMergedStream() {
   }
 }
 
-
-
 // web + computer screen + voice stream 
 
 // Share Computer Screen and Audio stream 
  async function shareScreen() {
+  
+    let screenAudioConstraints = {
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44100
+      },
+      video: false
+    };
     console.log("share Screen");
-    document.getElementById("error").innerHTML="";
-    voice = await micphone_status();
     screenStream = await computerscreen();
-    webcamer = await webcam_status();
-    computscreen = await computerScreen_status();
-    // Check if we need to add audio stream
-    let recordAudio = voice;
-    let cam = webcamer;
-    let compscreen = computscreen;
+    console.log("screenStream 239", screenStream);
+    let microphone = document.getElementById("audio-settings"); 
+    
     let stream = null;
-    if (recordAudio == true && cam == true && compscreen == true ) {
+    console.log("stream 240",stream);
+    stream = new MediaStream([...screenStream.getTracks()]);
+    if (microphone.checked == true ) {
       audioStream = await captureMediaDevices(screenAudioConstraints);
-      //stream = new MediaStream([...screenStream.getTracks(), ...audioStream.getTracks()]);
+      console.log("audioStream 243",audioStream);
       try {
         const mergeAudioStreams = (desktopStream, voiceStream) => {
           const context = new AudioContext();
+          console.log("desktopStream",desktopStream)
           // Create a couple of sources
           const source1 = context.createMediaStreamSource(desktopStream);
+  
           const source2 = context.createMediaStreamSource(voiceStream);
           const destination = context.createMediaStreamDestination();
           const desktopGain = context.createGain();
           const voiceGain = context.createGain();
-
+  
           desktopGain.gain.value = 0.7;
           voiceGain.gain.value = 0.7;
           // source 1 computer screen hai
           source1.connect(desktopGain).connect(destination);
           // Connect source2 voice hai
           source2.connect(voiceGain).connect(destination);
-          return destination.stream.getAudioTracks();
+          return destination.stream.getAudioTracks(); 
         };
-
-        //stream = mergeAudioStreams;
-        //stream = new MediaStream([...mergeAudioStreams.getTracks()]);
         const tracks = [
           ...screenStream.getVideoTracks(),
           ...mergeAudioStreams(screenStream, audioStream)
         ];
-
         console.log('Tracks to add to stream 167', tracks);
         stream = new MediaStream(tracks);
       } catch (error) {
         console.error("Error while creating merged audio streams: ", error)
-        stream = new MediaStream([...screenStream.getTracks(), ...audioStream.getTracks()]);
-      }
-
-    }  else if(recordAudio == true && screenStream == true){
-      audioStream = await captureMediaDevices(screenAudioConstraints);
-      //stream = new MediaStream([...screenStream.getTracks(), ...audioStream.getTracks()]);
-      try {
-        const mergeAudioStreams = (desktopStream, voiceStream) => {
-          const context = new AudioContext();
-          // Create a couple of sources
-          const source1 = context.createMediaStreamSource(desktopStream);
-          const source2 = context.createMediaStreamSource(voiceStream);
-          const destination = context.createMediaStreamDestination();
-          const desktopGain = context.createGain();
-          const voiceGain = context.createGain();
-
-          desktopGain.gain.value = 0.7;
-          voiceGain.gain.value = 0.7;
-          // source 1 computer screen hai
-          source1.connect(desktopGain).connect(destination);
-          // Connect source2 voice hai
-          source2.connect(voiceGain).connect(destination);
-          return destination.stream.getAudioTracks();
-        };
-
-        //stream = mergeAudioStreams;
-        //stream = new MediaStream([...mergeAudioStreams.getTracks()]);
-        const tracks = [
-          ...screenStream.getVideoTracks(),
-          ...mergeAudioStreams(screenStream, audioStream)
-        ];
-
-        console.log('Tracks to add to stream 167', tracks);
-        stream = new MediaStream(tracks);
-      } catch (error) {
-        console.error("Error while creating merged audio streams: ", error)
-        stream = new MediaStream([...screenStream.getTracks(), ...audioStream.getTracks()]);
       }
     }
-    else {
-      stream = new MediaStream([...screenStream.getTracks()]);
-      //stream = new MediaStream([...screenStream.getVideoTracks()]);
-    }
-    onCombineAvailable(stream);
-
+ 
+   onCombineAvailable(stream);
  }
-
 
  function onCombineAvailable(stream){
     console.log("onCombineAvailable 179");
@@ -346,7 +295,6 @@ function onBtnrecordClick() {
             var blob = new Blob (chunks , {    // chunks kisi b file ko save krny k liya use hota hai
                 type: "video/webm"  // isky bad semocolon nai lagana wrna error aay ga
               })
-              
             var videoUrl = window.URL.createObjectURL(blob);
             downloadlink.setAttribute("href",videoUrl);
             console.log("chunks array",chunks);
@@ -366,8 +314,6 @@ function onBtnrecordClick() {
         sharebtn.disabled = true;
         recbtn.disabled = true;
         stopbtn.disabled = false;
-    
-         
     }
  }
 
@@ -387,7 +333,7 @@ function onBtnrecordClick() {
 
 
 /*
-const video1 = document.getElementById('videocustom')
+const video1 = document.getElementById('vidsssssssseocustom')
  let mediaConstraints = {
     video: {
       width: 1280,
